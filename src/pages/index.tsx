fc2c5f9d6e3684c190a4536a1b12d0cdde03f6af
JamesL425 <79216118+JamesL425@users.noreply.github.com>
2025-05-19 23:16:56 +0100

    Update homepage content and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,10 +18,8 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+          <Link className="button button--secondary button--lg" to="/docs/intro">
+            Browse Colleges
           </Link>
         </div>
       </div>
@@ -33,8 +31,8 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Home | ${siteConfig.title}`}
+      description="Home page for the Cambridge CS Colleges Wiki">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
